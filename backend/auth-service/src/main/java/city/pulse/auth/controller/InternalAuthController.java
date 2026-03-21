25be09122baaa59ae3f21eeeb78b389d995377c4
denys1204 <denys.work.ua@gmail.com>
2026-03-21 11:54:53 +0100

    fix(auth-service): add user deletion functionality in AuthService

--- backend/auth-service/src/main/java/city/pulse/auth/controller/InternalAuthController.java
+++ backend/auth-service/src/main/java/city/pulse/auth/controller/InternalAuthController.java
@@ -6,10 +6,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -23,5 +20,11 @@ public class InternalAuthController {
     public ResponseEntity<UUID> createCredential(@Valid @RequestBody InternalRegistrationRequest dto) {
         var newUserId = service.registerUser(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(newUserId);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCredential(@PathVariable UUID id) {
+        service.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 }
