ba2aa1cbc06691344264271202841e5191669a52
denys1204 <denys.work.ua@gmail.com>
2026-03-22 15:34:46 +0100

    feat(user-service): add swagger support

--- backend/user-service/src/main/java/city/pulse/user/security/SecurityConfig.java
+++ backend/user-service/src/main/java/city/pulse/user/security/SecurityConfig.java
@@ -23,8 +23,8 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(basePath + "/internal/**").permitAll()
-                        .anyRequest().authenticated()
-                )
+                        .requestMatchers("/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
