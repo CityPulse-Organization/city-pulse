2b8ba82d3bf711c7d10ae03c9c3a385a029d864e
denys1204 <denys.work.ua@gmail.com>
2026-03-27 17:13:19 +0100

    refactor(storage-service): integrate common library for exception handling

--- backend/storage-service/src/main/java/city/pulse/storage/exception/GlobalExceptionHandler.java
+++ backend/storage-service/src/main/java/city/pulse/storage/exception/GlobalExceptionHandler.java
@@ -1,37 +1,16 @@
 package city.pulse.storage.exception;
 
+import city.pulse.common.exception.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex) {
-        List<ErrorResponse.ErrorDetails> errors = ex.getBindingResult()
-                .getFieldErrors()
-                .stream()
-                .map(fe -> new ErrorResponse.ErrorDetails(fe.getField(), fe.getDefaultMessage()))
-                .toList();
-
-        ErrorResponse errorResponse = new ErrorResponse(
-                LocalDateTime.now(),
-                HttpStatus.BAD_REQUEST.value(),
-                HttpStatus.BAD_REQUEST.getReasonPhrase(),
-                "Validation failed",
-                errors
-        );
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
-    }
 
     @ExceptionHandler(FileUploadingException.class)
     public ResponseEntity<ErrorResponse> handleFileUploadingException(FileUploadingException ex) {
@@ -45,13 +24,5 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(ErrorResponse.of(HttpStatus.BAD_REQUEST, "File is too large. Max size allowed is 10MB"));
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex) {
-        log.error("Unexpected error in storage-service: ", ex);
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR, "An internal server error occurred"));
     }
 }
